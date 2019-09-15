e00b151534d7c4b14a81854a6fe7e0f49941a487
Alex Domeshek <alex@labsuit.com>
2019-09-15 14:44:50 +0300

    A fixed, but long version of employees with their sub-teams

--- select-users-with-workers.sql
+++ select-users-with-workers.sql
@@ -1,7 +1,17 @@
 use my_supermarket;
 
-select u1.email, count(1)
-from users u1
-  left
-  join users u2 on u1.id = u2.boss_id
-group by u1.id
+(select
+   u1.email,
+   count(1)
+ from users u1
+   join users u2 on u1.id = u2.boss_id
+ group by u1.email)
+union (
+  select
+    email,
+    0
+  from users u1
+  where not exists(select 1
+                   from users u2
+                   where u1.id = u2.boss_id)
+);
